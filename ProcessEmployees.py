5677de0a5c2b78d2e5382d0b03b465a64f8ea810
EmilyHott4 <112112058+EmilyHott4@users.noreply.github.com>
2023-09-18 22:14:41 -0500

    fix issues

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -14,18 +14,35 @@ import csv
 
 #open the file
 
+Employee_Data = open("employee_data.csv","r")
 
+EmployeeData_Infile = csv.reader(Employee_Data, delimiter=",")
+
+skip = next(EmployeeData_Infile)
 
 
 #create an empty dictionary
 
+EmployeeNew_Salary = {}
 
 #use a loop to iterate through the csv file
 
-
+total_current = 0.0
+total_new = 0.0
+for r in EmployeeData_Infile:
     #check if the employee fits the search criteria
 
+    if r[9] == 'TS' and r[3] == 'Marketing' and r[4]=='CSR':
+        
+        emp_name = r[1]+' '+r[2]
+        current_salary = int(r[5])
+        total_current += current_salary
+        print(f'Employee Name: {emp_name} Current Salary: ${current_salary:.2f}')
 
+        up_salary = current_salary + (current_salary*.10)
+
+        EmployeeNew_Salary[emp_name] = up_salary
+        total_new += up_salary
     
 
 print()
@@ -34,16 +51,14 @@ print()
 
 #iternate through the dictionary and print out the key and value as per image
 
-
-
-
-          
+for rep in EmployeeNew_Salary:
+    print(f'Employee Name: {rep} New Salary: ${EmployeeNew_Salary[rep]:.2f}')
+         
 
 print()
 print('=========================================')
 print()
 
 #print out the total difference between the old salary and the new salary as per image.
-
-        
-    
+total_increase = total_new - total_current
+print(f'Total Increase in Salary:${total_increase:.2f}')
